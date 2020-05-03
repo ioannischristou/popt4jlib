fd9d25364f860badbbe266996ab26b25384a7e0a
ioannischristou <ichr@ait.edu.gr>
2020-05-03 18:48:28 +0300

    introduced new batch file to allow running the main optimizer (e.g. DGA) of OptFFNNRun in distributed mode without needing to call separately the commands to start the server and workers. Modified appropriately the params file for such distributed computation of the function evaluations.

---
+++ optffnnrundbatch.bat
@@ -0,0 +1,15 @@
+REM invoke as C:\path\to\popt4jlib> optffnnrundbatch.bat <params_file>
+REM starts 3 workers and one server in different windows, then starts the OptFFNNRun class in its own command prompt
+REM java <jvmargs> -cp <classpath> parallel.distributed.PDBTExecSingleCltWrkInitSrv [workers_port(7890)] [clients_port(7891)]
+call start PDBTExecSingleCltWrkInitSrv.bat 7890 7891
+echo Verify server has started before you press any key...
+pause
+REM java <jvm_args> -cp <classpath> parallel.distributed.PDBTExecInitedWrk [num_threads(10)] [serverhost(localhost)] [serverport(7890)] [dbglvl(0)]
+call start PDBTExecInitedWrk.bat 8 localhost 7890 1
+call start PDBTExecInitedWrk.bat 8 localhost 7890 1
+call start PDBTExecInitedWrk.bat 8 localhost 7890 1
+echo Verify all 3 workers are started before you press any key...
+pause
+REM java <jvm_args> -cp <classpath> popt4jlib.neural.OptFFNNRun <paramsfile>
+echo on
+java -Xmx4g -cp ./dist/popt4jlib.jar popt4jlib.neural.OptFFNNRun %1
