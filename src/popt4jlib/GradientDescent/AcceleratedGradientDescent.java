57c70c90cd31c0fcfb266247ee00d54baa5c8bba
ioannischristou <ichr@ait.edu.gr>
2020-06-13 17:40:21 +0300

    Added class AcceleratedGradientDescent in popt4jlib.GradientDescent package, implementing the so-named method.

---
+++ src/popt4jlib/GradientDescent/AcceleratedGradientDescent.java
@@ -0,0 +1,370 @@
+package popt4jlib.GradientDescent;
+
+import popt4jlib.LocalOptimizerIntf;
+import java.util.*;
+import utils.*;
+import analysis.*;
+import popt4jlib.*;
+
+/**
+ * Accelerated Gradient Descent method using the notion of "momentum" to avoid
+ * oscillations that plague steepest descent after the initial stage. Works 
+ * with guarantee when the objective function is an L-smooth function.
+ * See: Z. Lin et al: "Accelerated Optimization for Machine Learning: First 
+ * Order Algorithms", Springer, 2020.
+ * <p>Title: popt4jlib</p>
+ * <p>Description: A Parallel Meta-Heuristic Optimization Library in Java</p>
+ * <p>Copyright: Copyright (c) 2011-2020</p>
+ * <p>Company: </p>
+ * @author Ioannis T. Christou
+ * @version 1.0
+ */
+public class AcceleratedGradientDescent implements LocalOptimizerIntf {
+  HashMap _params;
+  private double _incValue=Double.MAX_VALUE;
+  private VectorIntf _inc=null;  // incumbent vector
+  FunctionIntf _f;
+  private transient AGDThread[] _threads=null;
+  private int _numOK=0;
+  private int _numFailed=0;
+
+
+  /**
+   * public no-arg constructor
+   */
+  public AcceleratedGradientDescent() {
+  }
+
+
+  /**
+   * public constructor. The parameters passed in the argument are copied
+   * in the data member _params so that later modifications to the argument
+   * do not affect this object or its methods.
+   * @param params HashMap
+   */
+  public AcceleratedGradientDescent(HashMap params) {
+    try {
+      setParams(params);
+    }
+    catch (Exception e) {
+      // no-op: cannot reach this point
+    }
+  }
+
+
+  /**
+   * return a copy of the parameters. Modifications to the returned object
+   * do not affect the data member.
+   * @return HashMap
+   */
+  public synchronized HashMap getParams() {
+    return new HashMap(_params);
+  }
+
+
+  /**
+   * return a new empty ArmijoSteepestDescent optimizer object (that must be
+   * configured via a call to setParams(p) before it is used.)
+   * @return LocalOptimizerIntf
+   */
+  public LocalOptimizerIntf newInstance() {
+    return new ArmijoSteepestDescent();
+  }
+
+
+  /**
+   * the optimization params are set to a copy of p
+   * @param p HashMap
+   * @throws OptimizerException if another thread is currently executing the
+   * <CODE>minimize(f)</CODE> method of this object.
+   */
+  public synchronized void setParams(HashMap p) throws OptimizerException {
+    if (_f!=null) 
+			throw new OptimizerException("cannot modify parameters while running");
+    _params = null;
+    _params = new HashMap(p);  // own the params
+  }
+
+
+  /**
+   * the main method of the class. Before it is called, a number of parameters
+   * must have been set (via the parameters passed in the constructor, or via
+   * a later call to setParams(p). These are:
+	 * <ul>
+   * <li> &lt;"agd.numthreads", Integer nt&gt; optional, the number of threads 
+	 * to use in the optimization process. Default is 1.
+	 * <li>&lt;"agd.numtries", Integer ntries&gt; optional, the number of initial 
+	 * starting points to use (must either exist then ntries 
+	 * &lt;"agd.x$i$",VectorIntf v&gt; pairs in the parameters or a pair 
+	 * &lt;"[gradientdescent.]x0",VectorIntf v&gt; pair in params). Default is 1.
+   * <li> &lt;"agd.gradient", VecFunctionIntf g&gt; optional, the gradient of f,
+   * the function to be minimized. If this param-value pair does not exist, the
+   * gradient will be computed using Richardson finite differences extrapolation
+   * <li> &lt;"agd.gtol", Double v&gt; optional, the minimum abs. value for each 
+	 * of the gradient's coordinates, below which if all coordinates of the 
+	 * gradient happen to be, the search stops assuming it has reached a 
+	 * stationary point. Default is 1.e-6.
+   * <li> &lt;"agd.maxiters", Integer miters&gt; optional, the maximum number of 
+	 * major iterations of the AGD search before the algorithm stops. Default is
+   * Integer.MAX_VALUE.
+   * <li> &lt;"agd.looptol", Double v&gt; optional, the minimum step-size 
+	 * allowed. Default is 1.e-21.
+	 * <li> &lt;"agd.L", double v&gt; optional, the L constant that is supposed to 
+	 * be the L-smoothness factor of the objective function. Default is 1.0.
+   * </ul>
+   * @param f FunctionIntf the function to minimize
+   * @throws OptimizerException if another thread is currently executing the
+   * same method or if the method fails to find a minimizer
+   * @return PairObjDouble the pair containing the arg. min (a VectorIntf) and
+   * the min. value found
+   */
+  public PairObjDouble minimize(FunctionIntf f) throws OptimizerException {
+		if (f==null) 
+			throw new OptimizerException("AcceleratedGradientDescent.minimize(f): "+
+				                           "null f");
+    try {
+      synchronized (this) {
+        if (_f != null)
+          throw new OptimizerException("AGD.minimize(): " +
+                                       "another thread is concurrently "+
+						                           "executing the method on this object");
+        _f = f;
+        _numOK=0;
+        _numFailed=0;
+        _inc = null;
+        _incValue = Double.MAX_VALUE;  // reset
+      }
+      int numthreads = 1;
+      try {
+        Integer ntI = (Integer) _params.get("agd.numthreads");
+        if (ntI != null && ntI.intValue() > 1) numthreads = ntI.intValue();
+      }
+      catch (ClassCastException e) { e.printStackTrace(); }
+      _threads = new AGDThread[numthreads];
+      int ntries = 1;
+      try {
+        Integer ntriesI = (Integer) _params.get("agd.numtries");
+        if (ntriesI != null && ntriesI.intValue() > 1) 
+					ntries = ntriesI.intValue();
+      }
+      catch (ClassCastException e) { e.printStackTrace(); }
+      int triesperthread = ntries / numthreads;
+      int rem = ntries;
+      for (int i = 0; i < numthreads - 1; i++) {
+        _threads[i] = new AGDThread(i, triesperthread);
+        rem -= triesperthread;
+      }
+      _threads[numthreads - 1] = new AGDThread(numthreads - 1, rem);
+      for (int i = 0; i < numthreads; i++) {
+        _threads[i].start();
+      }
+      // wait until all threads finish
+      for (int i=0; i<numthreads; i++) {
+        try {
+          _threads[i].join();
+        }
+        catch (InterruptedException e) {
+          Thread.currentThread().interrupt(); // recommended action
+        }
+      }
+      synchronized (this) {  // keep FindBugs happy
+        if (_inc == null) // didn't find a solution
+          throw new OptimizerException("failed to find solution");
+        // ok, we're done
+        PairObjDouble pr = new PairObjDouble(_inc, _incValue);
+        return pr;
+      }
+    }
+    finally {
+      synchronized (this) {
+        _f = null;
+      }
+    }
+  }
+
+
+  /**
+   * auxiliary method.
+   * @return int the number of "tries" that succeeded (found a stationary point)
+   */
+  public synchronized int getNumOK() { return _numOK; }
+  /**
+   * auxiliary method.
+   * @return int the number of tries that failed (did not find a saddle point)
+   */
+  public synchronized int getNumFailed() { return _numFailed; }
+
+
+  /**
+   * set the incumbent solution -if it is better than the current incumbent.
+   * @param arg VectorIntf proposed arg.
+   * @param val double proposed value
+   */
+  synchronized void setIncumbent(VectorIntf arg, double val) {
+    Messenger mger = Messenger.getInstance();
+		if (val<_incValue) {
+      mger.msg("update incumbent w/ new best value="+val,0);
+      _incValue=val;
+      _inc=arg;
+    }
+  }
+
+
+  /**
+   * introduced to keep FindBugs happy...
+   * @return FunctionIntf
+   */
+  synchronized FunctionIntf getFunction() { return _f; }
+
+
+  /**
+   * auxiliary method
+   */
+  synchronized void incOK() { _numOK++; }
+
+
+  /**
+   * auxiliary method
+   */
+  synchronized void incFailed() { _numFailed++; }
+
+  /** 
+	 * nested class implementing the threads to be used by ArmijoSteepestDescent 
+	 * main class
+	 */ 
+  class AGDThread extends Thread {
+
+    private int _numtries;
+    private int _id;
+    private int _uid;
+
+    public AGDThread(int id, int numtries) {
+      _id = id;
+      _uid = (int) DataMgr.getUniqueId();
+      _numtries=numtries;
+    }
+
+    public void run() {
+      HashMap p = getParams();  // was new HashMap(_master._params);
+      p.put("thread.localid", new Integer(_id));
+      p.put("thread.id", new Integer(_uid));  // used to be _id
+      VectorIntf best = null;
+      double bestval = Double.MAX_VALUE;
+      FunctionIntf f = getFunction();  // was _master._f;
+      for (int i=0; i<_numtries; i++) {
+        try {
+          int index = _id*_numtries+i;  // this is the starting point soln index
+          PairObjDouble pair = min(f, index, p);
+          double val = pair.getDouble();
+          if (val<bestval) {
+            bestval=val;
+            best=(VectorIntf) pair.getArg();
+          }
+          incOK();
+        }
+        catch (Exception e) {
+          incFailed();
+          e.printStackTrace();
+          // no-op
+        }
+      }
+      setIncumbent(best, bestval);
+    }
+
+
+    private PairObjDouble min(FunctionIntf f, int solindex, HashMap p) 
+			throws OptimizerException {
+			Messenger mger = Messenger.getInstance();
+			double L = 1.0;
+			if (p.containsKey("agd.L")) {
+				L = ((Double)p.get("agd.L")).doubleValue();
+			}
+			final double L_inv = 1.0/L;
+      VecFunctionIntf grad = (VecFunctionIntf) p.get("agd.gradient");
+      if (grad==null) grad = new GradApproximator(f);  // default: numeric 
+			                                                 // gradient computation
+    final VectorIntf x0 = 
+			_params.containsKey("agd.x"+solindex)==false ?
+         _params.containsKey("gradientdescent.x0") ? 
+			    (VectorIntf) _params.get("gradientdescent.x0") : 
+			      _params.containsKey("x0") ? (VectorIntf) _params.get("x0") : null 
+            // attempt to retrieve generic point
+			: (VectorIntf) _params.get("agd.x"+solindex);
+      if (x0==null) 
+				throw new OptimizerException("no agd.x"+solindex+
+					                           " initial point in _params passed");
+      VectorIntf x = x0.newInstance();  // don't modify the initial soln
+			VectorIntf xprev = x.newInstance();
+      final int n = x.getNumCoords();
+      double gtol = 1e-6;
+      Double gtolD = (Double) p.get("agd.gtol");
+      if (gtolD!=null && gtolD.doubleValue()>=0) gtol = gtolD.doubleValue();
+			
+			double thetaprev = 1.0;
+			double theta = 1.0;
+			double theta2 = 1.0;
+			double beta;
+      double fx = Double.NaN;
+      int maxiters = Integer.MAX_VALUE;
+      Integer miI = (Integer) p.get("agd.maxiters");
+      if (miI!=null && miI.intValue()>0)
+        maxiters = miI.intValue();
+      double looptol = 1.e-21;
+      Double ltD = (Double) p.get("agd.looptol");
+      if (ltD!=null && ltD.doubleValue() > 0) looptol = ltD.doubleValue();
+      boolean found=false;
+
+      fx = f.eval(x, p);
+      DblArray1Vector y = new DblArray1Vector(n);
+
+			// main iteration loop
+			
+      for (int iter=0; iter<maxiters; iter++) {
+	      mger.msg("AGDThread.min(): #iters="+iter+" fx="+fx,2);
+				if (Double.compare(gtol,0.0)>0) {
+					VectorIntf g = grad.eval(x, p);
+					double normg = VecUtil.norm(g,2);
+					mger.msg("AGDThread.min(): normg="+normg,2);
+					final double norminfg = VecUtil.normInfinity(g);
+	        if (norminfg <= gtol) {
+		        mger.msg("found sol w/ norminfg="+norminfg+" in "+
+							       iter+" iterations.",
+							       0);
+				    return new PairObjDouble(x, fx);
+					}
+				}
+				// set new theta, thetaprev, beta, and then y and x
+				beta = theta*(1-thetaprev)/thetaprev;
+				thetaprev = theta;
+				theta2 = theta*theta;
+				theta = (Math.sqrt(theta2*(theta2 + 4))-theta2)/2.0;
+				try {
+					for (int i=0; i<n; i++) {  // update y 
+						double yi = (beta+1)*x.getCoord(i) - beta*xprev.getCoord(i);
+						y.setCoord(i, yi);
+					}
+					// update x, xprev
+					VectorIntf g_y = grad.eval(y, p);
+					y.addMul(-L_inv, g_y);
+					//xprev = x;
+					for (int i=0; i<n; i++) xprev.setCoord(i, x.getCoord(i));
+					//x = y;
+					for (int i=0; i<n; i++) x.setCoord(i, y.getCoord(i));
+					fx = f.eval(x, p);
+				}
+				catch (Exception e) {  // cannot get here
+					e.printStackTrace();
+				}
+			}
+			
+      // end main iteration loop
+
+      if (found) return new PairObjDouble(x, fx);
+      else throw new OptimizerException("AGDThread did not find a solution"+
+                                        " satisfying tolerance criteria from "+
+                                        "the given initial point x0="+x0);
+    }
+
+  }
+}
+
